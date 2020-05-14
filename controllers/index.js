b5c5b64d4a6962c4fb9eb4ac9ad4a0ae63400b84
Janis Rancourt <janis.rancourt@gmail.com>
2020-05-14 11:42:32 -0400

    finished controllers

--- controllers/index.js
+++ controllers/index.js
@@ -15,7 +15,7 @@ const signUp = async (req, res) => {
  try {
   const { username, email, password } = req.body
   const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
-  const user = new User({
+  const user = await new User({
    username,
    email,
    password_digest
@@ -71,4 +71,77 @@ const verifyUser = async (req, res) => {
 } //end verifyUser
 
 //Product controllers
+
+const getProducts = async (req, res) => {
+ try {
+  const products = await Product.find()
+  res.json(products)
+ } catch (error) {
+  res.status(500).json({ error: error.message })
+ }
+}
+
+const getProduct = async (req, res) => {
+ try {
+  const { id } = req.params
+  const product = await Product.findById(id)
+  if (product) {
+   return res.json(product)
+  }
+  res.status(404).json({ message: 'Product not found!' })
+ } catch (error) {
+  res.status(500).json({ error: error.message })
+ }
+}
+
+const createProduct = async (req, res) => {
+ try {
+  const product = await new Product(req.body)
+  await product.save()
+  res.status(201).json(product)
+ } catch (error) {
+  res.status(500).json({ error: error.message })
+ }
+}
+
+const updateProduct = async (req, res) => {
+ const { id } = req.params
+ await Product.findByIdAndUpdate(id, req.body, { new: true }, (error, product) => {
+  if (error) {
+   return res.status(500).json({ error: error.message })
+  }
+  if (!product) {
+   return res.status(404).json({ message: 'Could not find this product.' })
+  }
+  res.status(200).json(product)
+ })
+}
+
+const deleteProduct = async (req, res) => {
+ try {
+  const { id } = req.params
+  const deleted = await Product.findByIdAndDelete(id)
+  if (deleted) {
+   return res.status(200).send("Product successfully removed")
+  }
+  throw new Error("Unable to find product.")
+ } catch (error) {
+  res.status(500).json({ error: error.message })
+ }
+}
+
+module.exports = {
+ signUp,
+ signIn,
+ verifyUser,
+ getProducts,
+ getProduct,
+ createProduct,
+ updateProduct,
+ deleteProduct
+}
+
+
+
+
 
