f6eba1f8381e95615af6883fe9a4a84ef209ed5b
Janis Rancourt <janis.rancourt@gmail.com>
2020-05-14 19:15:20 -0400

    wip version of page

---
+++ client/src/Admin/AdminHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddProduct from "./AddProduct/AddProduct"
+
+export default function AdminHome() {
+ return (
+  <div>
+   <AddProduct />
+  </div>
+ )
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.scss';
 import { Route } from "react-router-dom"
 import AdminHome from './Admin/AdminHome'
-import CustomerHome from './Customer/CustomerHome'
+// import CustomerHome from './Customer/CustomerHome'
 
 function App() {
  return (
   <div className="App">
-    <Route exact path="/" component={CustomerHome} />
-    <Route exact path="/admin" component={AdminHome} />
+   {/* <Route exact path="/" component={CustomerHome} /> */}
+   <Route exact path="/admin" component={AdminHome} />
   </div>
  );
 }
