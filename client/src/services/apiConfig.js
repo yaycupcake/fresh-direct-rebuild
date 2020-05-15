953b9de5e307fd84fde737efe9efe6901da28a4b
Scott Delbango <scott.delbango@gmail.com>
2020-05-14 23:49:22 -0400

    Work done on hooks-based validation, but not yet functional. Needs helper and services files set, and need to discuss pulling out form usage into custom hook

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const getToken = () => {
+  return new Promise(resolve => {
+    resolve(`Bearer ${localStorage.getItem('token') || null}`)
+  })
+}
+
+let apiUrl
+
+const apiUrls = {
+  production: 'TKTKTK',
+  development: 'http://localhost:3001/api'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+const api = axios.create({
+  baseURL: apiUrl
+})
+
+api.interceptors.request.use(async function (options) {
+  options.headers['Authorization'] = await getToken()
+  return options
+}, function (error) {
+  console.log('Request error: ', error)
+  return Promise.reject(error)
+});
+
+export default api
