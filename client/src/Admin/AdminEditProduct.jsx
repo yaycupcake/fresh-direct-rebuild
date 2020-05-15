db810053e52a4024f98513d3f99d81c995ce205d
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-05-15 14:24:13 -0400

    fixed useEffect and useState

--- client/src/Admin/AdminEditProduct.jsx
+++ client/src/Admin/AdminEditProduct.jsx
@@ -6,23 +6,29 @@ import Layout from '../shared/Layout'
 function AdminEditProduct() {
 
     const [product, setProduct] = useState({
-        brand: '',
-        productName: '',
-        size: '',
-        price: '',
-        unit: '',
-        category: '',
-        imgUrl: ''
+        brand: "",
+        productName: "",
+        size: "",
+        price: "",
+        unit: "",
+        salePrice: "",
+        unitPrice: "",
+        category: "",
+        imageUrls: ""
     })
 
 
     let { id } = useParams()
 
-    useEffect(() => {
+    const fetchProduct = async () => {
         const product = await getProduct(id)
         setProduct({ product })
+    }
 
+    useEffect(() => {
+        fetchProduct();
     })
+
 
     function handleChange(event) {
         const { name, value } = event.target
@@ -36,74 +42,74 @@ function AdminEditProduct() {
     }
 
 
-  return (
-      <Layout user={props.user}>
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="admin-update-brand"
-                    placeholder="Brand"
-                    value={product.brand}
-                    name="brand"
-                    required
-                    onChange={handleChange}
-                />
-                <input
-                    className="admin-update-productName"
-                    placeholder="Product Name"
-                    value={product.productName}
-                    name="productName"
-                    required
-                    onChange={handleChange}
-                />
-                <input
-                    className="admin-update-size"
-                    placeholder="Size"
-                    value={product.size}
-                    name="size"
-                    required
-                    onChange={handleChange}
-                />
-                <input
-                    className="admin-update-price"
-                    placeholder="price"
-                    value={product.price}
-                    name="price"
-                    required
-                    onChange={handleChange}
-                />
-                <input
-                    className="admin-update-unit"
-                    placeholder="Unit"
-                    value={product.unit}
-                    name="unit"
-                    required
-                    onChange={handleChange}
-                />
-                <input
-                    className="admin-update-category"
-                    placeholder="Category"
-                    value={product.category}
-                    name="category"
-                    required
-                    onChange={handleChange}
-                />
-            </form>
-            <form>
-                <input
-                    className="admin-update-imageUrl"
-                    placeholder='Image Url'
-                    value={product.imgUrl}
-                    name='imgUrl'
-                    type='text'
-                    required
-                    onChange={handleChange}
-                />
-            </form>
-            <button type="submit">Save</button>
+    return (
+        <Layout user={props.user}>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        className="admin-update-brand"
+                        placeholder="Brand"
+                        value={product.brand}
+                        name="brand"
+                        required
+                        onChange={handleChange}
+                    />
+                    <input
+                        className="admin-update-productName"
+                        placeholder="Product Name"
+                        value={product.productName}
+                        name="productName"
+                        required
+                        onChange={handleChange}
+                    />
+                    <input
+                        className="admin-update-size"
+                        placeholder="Size"
+                        value={product.size}
+                        name="size"
+                        required
+                        onChange={handleChange}
+                    />
+                    <input
+                        className="admin-update-price"
+                        placeholder="price"
+                        value={product.price}
+                        name="price"
+                        required
+                        onChange={handleChange}
+                    />
+                    <input
+                        className="admin-update-unit"
+                        placeholder="Unit"
+                        value={product.unit}
+                        name="unit"
+                        required
+                        onChange={handleChange}
+                    />
+                    <input
+                        className="admin-update-category"
+                        placeholder="Category"
+                        value={product.category}
+                        name="category"
+                        required
+                        onChange={handleChange}
+                    />
+                </form>
+                <form>
+                    <input
+                        className="admin-update-imageUrl"
+                        placeholder='Image Url'
+                        value={product.imgUrl}
+                        name='imgUrl'
+                        type='text'
+                        required
+                        onChange={handleChange}
+                    />
+                </form>
+                <button type="submit">Save</button>
 
-      </div>
-      </Layout>
+            </div>
+        </Layout>
     )
 }
 export default AdminEditProduct 
