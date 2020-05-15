17b939b30053fc78a1a8ca9e46f902fbf46401db
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-05-15 14:27:19 -0400

    fixed file structure for AdminEditProduct

--- client/src/Admin/AdminEditProduct/AdminEditProduct.jsx
+++ client/src/Admin/AdminEditProduct/AdminEditProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
-import { getProduct, updateProduct } from '../services/product'
+import { getProduct, updateProduct } from '../../services/product'
 import Layout from '../shared/Layout'
 
 function AdminEditProduct() {
