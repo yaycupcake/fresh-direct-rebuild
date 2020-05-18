97e0a597737401d0fff9d491cdd52861a7127da0
Janis Rancourt <janis.rancourt@gmail.com>
2020-05-18 09:52:17 -0400

    making sample scss

--- client/src/Admin/AdminHome.jsx
+++ client/src/Admin/AdminHome.jsx
@@ -12,30 +12,30 @@ import { verifyUser } from '../services/user'
 
 const AdminHome = () => {
 
-  const [user, setUser] = useState(null)
+ const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    const userEffect = async () => {
-      const verifiedUser = await verifyUser()
-      if (verifiedUser) {
-        setUser(verifiedUser.user)
-      }
-    }
-    userEffect()
-  }, [])
+ useEffect(() => {
+  const userEffect = async () => {
+   const verifiedUser = await verifyUser()
+   if (verifiedUser) {
+    setUser(verifiedUser.user)
+   }
+  }
+  userEffect()
+ }, [])
 
-  return (
-     <Switch>
-      <Route exact path='/admin/products' render={() => {return <ProductCards user={user} />}} />
-      {/* <Route exact path='/admin/products/:id' render={(props) => <ProductDetail {...props} user={user} history={props.history} />} /> */}
-      <Route exact path="/admin/products/:id/edit" render={(props) => user ? <AdminEditProduct {...props} user={user} /> : <Redirect to='/admin' />} />
-      <Route exact path="/admin/add-product" render={() => user ? <AddProduct user={user} /> : < Redirect to='/admin/sign-up' />} />
-      <Route exact path="/admin/sign-up" render={props => <AdminSignUp setUser={setUser} history={props.history} />} />
-      <Route exact path="/admin/sign-in" render={props => <AdminLogIn setUser={setUser} history={props.history} />} />
-      <Route exact path="/admin/sign-out" render={props => <AdminSignOut user={user} clearUser={setUser} history={props.history} />} />
-      <Route exact path='/admin' render={() => <h1>Home</h1> } />
-    </Switch>
-  )
+ return (
+  <Switch>
+   <Route exact path='/admin/products' render={() => { return <ProductCards user={user} /> }} />
+   {/* <Route exact path='/admin/products/:id' render={(props) => <ProductDetail {...props} user={user} history={props.history} />} /> */}
+   <Route exact path="/admin/products/:id/edit" render={(props) => user ? <AdminEditProduct {...props} user={user} /> : <Redirect to='/admin' />} />
+   <Route exact path="/admin/add-product" render={() => user ? <AddProduct user={user} /> : < Redirect to='/admin/sign-up' />} />
+   <Route exact path="/admin/sign-up" render={props => <AdminSignUp setUser={setUser} history={props.history} />} />
+   <Route exact path="/admin/sign-in" render={props => <AdminLogIn setUser={setUser} history={props.history} />} />
+   <Route exact path="/admin/sign-out" render={props => <AdminSignOut user={user} clearUser={setUser} history={props.history} />} />
+   <Route exact path='/admin' render={() => <h1>Home</h1>} />
+  </Switch>
+ )
 }
 
 export default AdminHome
