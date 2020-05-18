3d9050cd24b6e98f858801faabb65a5a6e0f7928
Scott Delbango <scott.delbango@gmail.com>
2020-05-18 16:45:29 -0400

    Added fields in product info and edit page

--- client/src/Admin/AdminProductDetail/AdminProductDetail.jsx
+++ client/src/Admin/AdminProductDetail/AdminProductDetail.jsx
@@ -22,16 +22,17 @@ export default function ProductDetail(props) {
       <div className='current-image'>
         <h1>Product Details</h1>
         <ul className="info-list">
-          <li className='info-line'>Brand: {product && product.brand}</li>
-          <li className='info-line'>Name: {product && product.productName}</li>
-          <li className='info-line'>Size: {product && product.size}</li>
-          <li className='info-line'>Price: {product && product.price}</li>
-          <li className='info-line'>Unit: {product && product.unit}</li>
-          <li className='info-line'>Sale Price: {product && product.salePrice}</li>
-          <li className='info-line'>Unit Price: {product && product.unitPrice}</li>
-          <li className='info-line'>Category: {product && product.category}</li>
-          <li className='info-line'>Image URLs: {product && product.imageUrls}</li>
+          <li className='info-line'>Brand: <input value={product && product.brand} className="info-field" type="text" /></li>
+          <li className='info-line'>Name: <input value={product && product.productName} className="info-field" type="text" /></li>
+          <li className='info-line'>Size: <input value={product && product.size} className="info-field" type="text" /></li>
+          <li className='info-line'>Price: <input value={product && product.price} className="info-field" type="text" /></li>
+          <li className='info-line'>Unit: <input value={product && product.unit} className="info-field" type="text" /></li>
+          <li className='info-line'>Sale Price: <input value={product && product.salePrice} className="info-field" type="text" /></li>
+          <li className='info-line'>Unit Price: <input value={product && product.unitPrice} className="info-field" type="text" /></li>
+          <li className='info-line'>Category: <input value={product && product.category} className="info-field" type="text" /></li>
+          <li className='info-line'>Image URLs: <input value={product && product.imageUrls} className="info-field" type="text" /></li>
         </ul>
+        <button type='submit' className='save'>Save</button>
       </div>
       <div className='thumbnail-container'></div>
     </div>
