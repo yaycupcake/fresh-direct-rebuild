4c5ea9ce6ed5c95b55fc65327ea74bb2b3270ebb
Jenna Rysanek <jennarysanek@jennas-mbp.lan>
2020-05-19 12:27:17 -0400

    Created Message Component Created Message.scss Added information and styling to Component

--- client/src/Customer/CustomerHome.jsx
+++ client/src/Customer/CustomerHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Footer from './shared/Footer'
 import Nav from './shared/Nav'
+import Message from './components/Message'
 
 export default function CustomerHome() {
   return (
     <div>
       <Nav />
       <h1>This is a cool Homepage</h1>
+      <Message />
       <Footer />
     </div>
   )
---
+++ client/src/Customer/components/Message.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Message.scss'
+
+export default function Message() {
+    return (
+        <div className="Message">
+            <h3> Meals for a Good Cause</h3>
+            <p> These hearty, homestyle entrees, veggies,
+            and soups from Restaurant Associates
+            Kitchen are more than just a great, fully-
+            prepped meal option. For every meal</p>
+            <a href="#">Read More</a>
+
+        </div>
+    )
+}
