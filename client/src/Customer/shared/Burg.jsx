b8abb52c95c0fc89173743d8713a5a47420ae855
Scott Delbango <scott.delbango@gmail.com>
2020-05-21 12:37:15 -0400

    icons added

--- client/src/Customer/shared/Burg.jsx
+++ client/src/Customer/shared/Burg.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { slide as Menu } from 'react-burger-menu'
 import './Burg.scss'
 import logo from '../../assets/icons/logo.png'
+import carrot from '../../assets/icons/carrot-icon.png'
+import home from '../../assets/icons/home-icon.png'
+import reorder from '../../assets/icons/reorder-icon.png'
+import shop from '../../assets/icons/shop-icon.png'
+import x from '../../assets/icons/x-icon.png'
+import foodkick from '../../assets/icons/foodkick-icon.png'
+
 
 export default function Burg() {
 
@@ -12,13 +19,13 @@ export default function Burg() {
   return (
     <Menu className='dropdown'>
       <a id="home" className="menu-item-frame" href="/"><img id='menu-logo' src={logo} /></a>
-      <a id="shop" className="menu-item" href="/shop">Shop</a>
-      <a id="about" className="menu-item" href="/about">Reorder</a>
-      <a id="more" className="menu-item" href="/more">More Ways to Shop</a>
+      <a id="shop" className="menu-item" href="/shop"><img src={home} />Shop</a>
+      <a id="about" className="menu-item" href="/about"><img src={reorder} />Reorder</a>
+      <a id="more" className="menu-item" href="/more"><img src={shop} />More Ways to Shop</a>
       <a onClick={showSettings} className="menu-item--small" href="">Settings</a>
       <a id="contact" className="menu-item" href="/contact">Contact Us</a>
       <a id="help" className="menu-item" href="/help">Help</a>
-      <a id="foodkick" className="menu-item-frame" href="/foodkick">FOODKICK</a>
+      <a id="foodkick" className="menu-item-frame" href="/foodkick"><img id='foodkick-logo' src={foodkick} /></a>
     </Menu>
   )
 }
